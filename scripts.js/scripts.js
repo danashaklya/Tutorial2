0c8c296b3029f4583cf40621973a67ba71d0acf2
Dana <dandonshak@gmail.com>
2021-09-23 20:25:29 -0400

    firsst commit

---
+++ scripts.js/scripts.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMConentLoaded", initializeApp);
+
+function initializeApp() {
+
+}
+
+async function displayMeme() {
+
+try{
+    const response = await
+    fetch("https://api.imgflip.com/get_memes");
+    const jsonResponse = await response.json();
+    //required
+    console.log(jsonResponse);
+    const meme = jsonResponse.data.memes[1];
+
+    const image = document.createElement("img");
+    image.src = meme.url;
+    image.alt = meme.name;
+    image.width = 200;
+
+    const memeContainer =
+        document.querySelector("#meme");
+    memeContainer.appendChild(image);
+    }catch (error)  {
+        console.log("Promise failed", error);
+    }
+}
